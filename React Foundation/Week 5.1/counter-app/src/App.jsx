a9c69b9ad58fc6eb192d03aef19ec29af3973e8e
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-04-28 15:46:41 +0530

    updated  counter code

--- React Foundation/Week 5.1/counter-app/src/App.jsx
+++ React Foundation/Week 5.1/counter-app/src/App.jsx
@@ -7,50 +7,34 @@ import './App.css'
 // }
 //jsx:javascript xml
 
-function App() {
-  const [todos, setTodos] = useState([{
-  title:"Go to Gym",
-  description: "Go to gym from 7-9 ",
-  completed:false
-},
-{
-  title:"Study DSA",
-  description: "Study DSA from 9-11",
-  completed:true
-}])
 
-function addTodo(){
-  setTodos([...todos,{
-title:"new todo",
-description:"descrition of new todo"
-  }])
-}
-  return (
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+function onClickHandler(){
+  setCount(count+1);
+  }
+
+  return(
     <>
     <div>
- <button onClick={addTodo}>Add a Random Todo</button>
- {
-  todos.map(function(todo,index){
-    return <Todo key={index} title={todo.title} description={todo.description}/>
-  })
- }
+     <Counter count={count}/>
     </div>
     </>
   )
 
+  
+function Counter(props){
+  return(
+    <>
+     <button style={{background:"balck",color:"white"}} onClick={onClickHandler}>Counter:{props.count}</button>
+    </>
+  )
+}
 }
 
-//todo app
-//title
-//description
-//complete
-function Todo(props){
-return(
-<div>
-  <h1>{props.title}</h1>
-  <p>{props.description}</p>
-</div>
-)
-}
+
+
 
 export default App
