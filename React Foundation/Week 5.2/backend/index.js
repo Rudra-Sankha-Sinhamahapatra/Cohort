7434a137c787f1bde70bde7a2bff0d606adc968d
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-04-28 20:05:33 +0530

    updated

--- React Foundation/Week 5.2/backend/index.js
+++ React Foundation/Week 5.2/backend/index.js
@@ -1,12 +1,21 @@
 
 import express from "express"
+import {createTodo,updateTodo} from "./types"
 
 const app=express();
 
 app.use(express.json())
 
 app.post("/todo",function(req,res){
+const createPayload=req.body;
+const parsePayload=createTodo.safeParse(createPayload);
 
+if(!parsePayload.success){
+res.status(411).json({
+    msg:"You sent the wrong inputs",
+})
+return;
+}
 })
 
 app.get("/todos",function(req,res){
@@ -14,5 +23,12 @@ app.get("/todos",function(req,res){
 })
 
 app.put("/completed",function(req,res){
-
+const updatePayload=req.body;
+const parsePayload=updateTodo.safeParse(updatePayload);
+if(!parsePayload.success){
+    res.status(411).json({
+        msg:"You have given wrong inputs",
+    })
+    return;
+}
 })
