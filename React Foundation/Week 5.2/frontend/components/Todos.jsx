27b22c6eb5c45fd96a60081763ea49163a2ed059
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-01 15:34:54 +0530

    added useEffect

--- React Foundation/Week 5.2/frontend/components/Todos.jsx
+++ React Foundation/Week 5.2/frontend/components/Todos.jsx
@@ -7,14 +7,14 @@ todos=[
 ]
 */
 
-export function Todos({todos}){
+export function Todos({todos},{index}){
     return(
         <>
-        <div>
-         {todos.map(function(todo){
+        <div key={index}>
+         {todos.map(function(todo,id){
             return(
-                <>
-                <div>
+                <> 
+                <div key={id}>
                     <h1>{todo.title}</h1>
                     <h2>{todo.description}</h2>
                     <button>{todo.completed==true?"Completed":"Mark as Complete"}</button>
--- React Foundation/Week 5.2/frontend/src/App.jsx
+++ React Foundation/Week 5.2/frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { CreateTodo } from '../components/CreateTodo'
 import { Todos } from '../components/Todos'
 
+let index=1;
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("https://localhost:3000/todos")
-  .then(async function(res){
-    const json=await res.json();
-    setTodos(json.todos);
-  })
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos")
+    .then(async function(res){
+      const json=await res.json();
+      setTodos(json.todos);
+    })
+  },[])
+
   return (
     <>
 <CreateTodo/>
-<Todos todos={todos}/>
+<Todos  index={index++} todos={todos}/>
     </>
   )
 }
