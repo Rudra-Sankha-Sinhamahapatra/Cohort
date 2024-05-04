33937a4ec153a2dfe49a6d54c0a340205c22c471
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-04 06:46:52 +0530

    done with useMemo

---
+++ React Foundation/Week 6.4 Assignments/usememo/components/Assignment1.jsx
@@ -0,0 +1,31 @@
+import { useMemo, useState } from "react";
+
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
+// Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
+
+export function Assignment1() {
+    const [input, setInput] = useState(0);
+    // Your solution starts here
+    const expensiveValue=useMemo(()=>{
+        let fact=1;
+   let i=input;
+
+ while(i>1){
+    fact*=i
+    i--;
+ }   
+        return fact;
+      },[input]);
+    // Your solution ends here
+
+    return (
+        <div>
+            <input 
+                type="number" 
+                value={input} 
+                onChange={(e) => setInput(Number(e.target.value))} 
+            />
+            <p>Calculated Value: {expensiveValue}</p>
+        </div>
+    );
+}
---
+++ React Foundation/Week 6.4 Assignments/usememo/components/Assignment3.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useMemo } from 'react';
+// You have been given a list of items you shopped from the grocery store
+// You need to calculate the total amount of money you spent
+
+export const Assignment3 = () => {
+    const [items, setItems] = useState([
+        { name: 'Chocolates', value: 10 },
+        { name: 'Chips', value: 20 },
+        { name: 'Onion', value: 30 },
+        { name: 'Tomato', value: 30 },
+        { name: 'Pizza', value: 300 },
+        { name: 'Burger', value: 200 },
+        // Add more items as needed
+    ]);
+
+    // Your code starts here
+    const totalValue = useMemo(()=>{
+        let val=0;
+   for(var i=0;i<items.length;i++){
+    val+=items[i].value;
+   }
+   return val;
+    },[items])
+    // Your code ends here
+    return (
+        <div>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item.name} - Price: ${item.value}</li>
+                ))}
+            </ul>
+            <p>Total Value: {totalValue}</p>
+        </div>
+    );
+};
