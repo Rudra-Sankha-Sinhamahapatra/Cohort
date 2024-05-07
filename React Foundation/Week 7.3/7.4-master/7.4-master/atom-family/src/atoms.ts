4b0a78d05e42cd9e8b55555cf74c0543636c9c47
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-07 12:01:37 +0530

    ok

--- React Foundation/Week 7.3/7.4-master/7.4-master/atom-family/src/atoms.ts
+++
@@ -1,9 +0,0 @@
-import { atomFamily } from "recoil";
-import { TODOS } from "./todos";
-
-export const todosAtomFamily = atomFamily({
-  key: 'todosAtomFamily',
-  default: id => {
-    return TODOS.find(x => x.id === id)
-  },
-});
