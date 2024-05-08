efb70734387ad7199d77577640d277fd76703ac5
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-08 22:54:16 +0530

    formatted document

--- React Foundation/Week7 Assignments/Assignment 4/src/Input.jsx
+++ React Foundation/Week7 Assignments/Assignment 4/src/Input.jsx
@@ -4,55 +4,66 @@ import { inputAtom, wordAtom } from "./atoms";
 const words = ["hi", "my", "name", "is", "for", "to", "random", "word"];
 
 export function Input() {
-    const [num, setNum] = useRecoilState(inputAtom);
-    const [word, setWord] = useRecoilState(wordAtom);
+  const [num, setNum] = useRecoilState(inputAtom);
+  const [word, setWord] = useRecoilState(wordAtom);
 
-    const generateParagraph =() => {
-        let paragraph = "";
-        for (let i = 0; i < num; i++) {
-            const randomIndex = Math.floor(Math.random() * words.length); // Generate a random index
-            paragraph += words[randomIndex] + " ";
-        }
-        setWord(paragraph.trim());
-    };
+  const generateParagraph = () => {
+    let paragraph = "";
+    for (let i = 0; i < num; i++) {
+      const randomIndex = Math.floor(Math.random() * words.length); // Generate a random index
+      paragraph += words[randomIndex] + " ";
+    }
+    setWord(paragraph.trim());
+  };
 
-
-    return (
-        <div>
-            <h1
-            style={{
-                display:"flex",
-                justifyContent:"center"
-            }}
-            >Para Generator</h1> <br />
-            <input
-            style={{
-                height:"40px",
-                width:"900px",
-                position:"relative",
-                left:"40px"
-            }}
-                type="number"
-                placeholder="Enter the Number of words"
-                value={num}
-                onChange={(e) => {
-                    setNum(e.target.value);
-                }}
-            />
-            <button
-                style={{ backgroundColor: "black", color: 'white', position: 'relative', left: "60px", height: "40px",width:"100px",borderRadius:"30px" }}
-                onClick={generateParagraph} // Call generateParagraph function on button click
-            >
-                Generate
-            </button>
-            <div
-            style={{
-                position:'relative',
-                left:"40px",
-                top:"50px"
-            }}>
-                {word}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Para Generator
+      </h1>{" "}
+      <br />
+      <input
+        style={{
+          height: "40px",
+          width: "900px",
+          position: "relative",
+          left: "40px",
+        }}
+        type="number"
+        placeholder="Enter the Number of words"
+        value={num}
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      />
+      <button
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          position: "relative",
+          left: "60px",
+          height: "40px",
+          width: "100px",
+          borderRadius: "30px",
+        }}
+        onClick={generateParagraph} // Call generateParagraph function on button click
+      >
+        Generate
+      </button>
+      <div
+        style={{
+          position: "relative",
+          left: "40px",
+          top: "50px",
+        }}
+      >
+        {word}
+      </div>
+    </div>
+  );
 }
