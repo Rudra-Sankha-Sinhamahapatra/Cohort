e6a11cbcd603267b27e986980b869a9594161adf
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-08 22:51:20 +0530

    updated para generator

---
+++ React Foundation/Week7 Assignments/Assignment 4/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { RecoilRoot } from "recoil";
+import { Input } from "./Input";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Input />
+    </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ React Foundation/Week7 Assignments/Assignment 4/src/atoms.js
@@ -0,0 +1,11 @@
+import {atom} from 'recoil'
+
+export const inputAtom=atom({
+key:"inputAtom",
+default:null
+});
+
+export const wordAtom=atom({
+    key:"wordAtom",
+    default:""
+})
