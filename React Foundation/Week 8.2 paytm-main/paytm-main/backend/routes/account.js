32f7a69e08e04a0378e293d53f7e2ed8494765c6
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-11 07:19:59 +0530

    updated

--- React Foundation/Week 8.2 paytm-main/paytm-main/backend/routes/account.js
+++ React Foundation/Week 8.2 paytm-main/paytm-main/backend/routes/account.js
@@ -2,6 +2,7 @@ const express=require('express');
 const { authMiddleware } = require('../middleware');
 const { Account } = require('../db');
 const router=express.Router();
+const mongoose=require('mongoose')
 
 router.get('/balance',authMiddleware,async(req,res)=>{
 const account=await Account.findOne({
