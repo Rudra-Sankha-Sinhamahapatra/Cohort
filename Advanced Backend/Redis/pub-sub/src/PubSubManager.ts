9729b85a5529f3b13ce31a5e8e599a84f82a1eee
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-24 04:10:45 +0530

    Added

---
+++ Advanced Backend/Redis/pub-sub/src/PubSubManager.ts
@@ -0,0 +1,54 @@
+import { createClient, RedisClientType } from "redis";
+
+export class PubSubManager {
+  private static instance: PubSubManager;
+  private redisClient:RedisClientType;
+  private subscriptions:Map<string,string[]>;
+  
+  private constructor() {
+   this.redisClient=createClient();
+   this.redisClient.connect();
+   this.subscriptions=new Map();
+  }
+
+  public static getInstance(): PubSubManager {
+    if (!PubSubManager.instance) {
+      PubSubManager.instance = new PubSubManager();
+    }
+    return PubSubManager.instance;
+  }
+
+public userSubscribe(userId:string,stock:string){
+  if(!this.subscriptions.has(stock)){
+    this.subscriptions.set(stock,[]);
+  }
+  this.subscriptions.get(stock)?.push(userId);
+
+  if(this.subscriptions.get(stock)?.length===0){
+    this.redisClient.subscribe(stock,(message)=>{
+      this.handleMessage(stock,message);
+    })
+    console.log(`Subscribed to Redis Channel: ${stock}`);
+  }
+}
+
+public userUnsubscribe(userId:string,stock:string){
+  this.subscriptions.set(stock,this.subscriptions.get(stock)?.filter((sub)=>sub!==userId)||[]);
+
+  if(this.subscriptions.get(stock)?.length===0){
+  this.redisClient.unsubscribe(stock);
+  console.log(`Unsubscribed to Redis Channel:${stock}`);
+  }
+}
+
+private handleMessage(stock:string,message:string){
+  console.log(`Message received on channel ${stock}:${message}`);
+  this.subscriptions.get(stock)?.forEach((sub)=>{
+    console.log(`Sending message to user:${sub}`);
+  })
+}
+
+public async disconnect(){
+  await this.redisClient.quit();
+}
+}
