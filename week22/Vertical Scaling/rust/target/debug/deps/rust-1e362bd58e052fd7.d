7b6a0f543ff4bb12ac9387409b0fa770865094d2
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-30 08:43:01 +0530

    init

---
+++ week22/Vertical Scaling/rust/target/debug/deps/rust-1e362bd58e052fd7.d
@@ -0,0 +1,5 @@
+C:\Coding by Rudra - Github\Cohort\week22\Vertical Scaling\rust\target\debug\deps\rust-1e362bd58e052fd7.exe: src/main.rs
+
+C:\Coding by Rudra - Github\Cohort\week22\Vertical Scaling\rust\target\debug\deps\rust-1e362bd58e052fd7.d: src/main.rs
+
+src/main.rs:
---
+++ week22/Vertical Scaling/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+C:\Coding\ by\ Rudra\ -\ Github\Cohort\week22\Vertical\ Scaling\rust\target\debug\rust.exe: C:\Coding\ by\ Rudra\ -\ Github\Cohort\week22\Vertical\ Scaling\rust\src\main.rs
