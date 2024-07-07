0545038cecdb348afea765f4ae1352c53312d73e
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-07-07 07:38:08 +0530

    Bug fixed

--- webrtc/backend/src/index.ts
+++ webrtc/backend/src/index.ts
@@ -10,18 +10,22 @@ wss.on('connection',function connection(ws){
     ws.on('message',function message(data:any){
         const message=JSON.parse(data);
         if(message.type==='sender'){
+            console.log("Sender set");
             senderSocket=ws;
         } else if(message.type==='receiver'){
+            console.log("Receiver set");
             receiverSocket=ws;
         } else if(message.type==='createOffer'){
             if(ws!==senderSocket){
                 return;
             }
+            console.log("Offer received");
          receiverSocket?.send(JSON.stringify({type:'createOffer',sdp:message.sdp}));
         } else if(message.type==='createAnswer'){
-            if(ws!=receiverSocket){
+            if(ws!==receiverSocket){
                 return;
             }
+            console.log("Answer Received");
             senderSocket?.send(JSON.stringify({type:'createAnswer',sdp:message.sdp}));
         } else if(message.type==='iceCandidate'){
             if(ws===senderSocket){
