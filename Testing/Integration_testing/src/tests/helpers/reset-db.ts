5d6f745cd8fb9500d8094f0c56a905d252725be7
Rudra-Sankha-Sinhamahapatra <rssmp120@.com>
2024-09-27 02:27:53 +0530

    testing added with cyprus

---
+++ Testing/Integration_testing/src/tests/helpers/reset-db.ts
@@ -0,0 +1,10 @@
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async () => {
+  await prisma.$transaction([
+    prisma.request.deleteMany(),
+  ])
+}
