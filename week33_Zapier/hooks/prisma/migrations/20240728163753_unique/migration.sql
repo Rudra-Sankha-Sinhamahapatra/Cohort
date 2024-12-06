376e4cd4322259cf9eae4aa689dc347bd2b5d19a
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-12-06 20:50:44 +0530

    adding

--- week33_Zapier/hooks/prisma/migrations/20240728163753_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[zapRunId]` on the table `ZapRunOutBox` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ZapRunOutBox_zapRunId_key" ON "ZapRunOutBox"("zapRunId");
